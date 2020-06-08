d29b6e7a998f1089b629a9108b9e7bc6d129a90e
kiki-doyouloveme <58351085+kiki-doyouloveme@users.noreply.github.com>
2020-06-08 08:08:27 +0800

    Create constnumber.h

---
+++ constnumber.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef _CONSTNUMBER_
+#define _CONSTNUMBER_
+
+#include "cocos2d.h"
+
+#define HEALTH 6
+#define MAGIC 200				  
+#define ARMOR 5
+#define SPEED 1
+
+enum Camp
+{
+	ME,
+	ENEMY
+};
+
+#endif //_CONSTNUMBER_ 
