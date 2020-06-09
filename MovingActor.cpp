72136d291d7d4ecfe8e71b7cebd5c6f20ab1645f
kiki-doyouloveme <58351085+kiki-doyouloveme@users.noreply.github.com>
2020-06-09 14:09:43 +0800

    Update MovingActor.cpp

--- MovingActor.cpp
+++ MovingActor.cpp
@@ -1,12 +1,11 @@
 #include "MovingActor.h"
-#include "cocos2d.h"
 
 USING_NS_CC;
 
-MovingActor* MovingActor::create(const std::string& filename, Camp actorcamp)
+MovingActor* MovingActor::create(HelloWorld* combatScene, const std::string& filename, Camp actorcamp)
 {
-	MovingActor* sprite = new (std::nothrow) MovingActor();
-	if (sprite && sprite->init(filename, actorcamp))
+	MovingActor* sprite = new (std::nothrow) MovingActor;
+	if (sprite && sprite->init(combatScene, filename, actorcamp))
 	{
 		sprite->autorelease();
 		return sprite;
@@ -15,13 +14,20 @@ MovingActor* MovingActor::create(const std::string& filename, Camp actorcamp)
 	return nullptr;
 }
 
-bool MovingActor::init(const std::string& filename, Camp actorcamp)
+bool MovingActor::init(HelloWorld* combatScene, const std::string& filename, Camp actorcamp)
 {
 	if (!Sprite::initWithFile(filename))
 		return false;
-	setActorCamp(actorcamp);
-	_actorState->init();
+	MovingActor::initstate(actorcamp);
 	return true;
+}
+
+void MovingActor::initstate(Camp actorcamp)
+{
+	setActorCamp(actorcamp);
+	setActorWeapon1(nullptr);
+	setActorWeapon2(nullptr);
+	_actorState->init();
 }
 
 void MovingActor::die()
@@ -44,9 +50,9 @@ void MovingActor::damage(INT32 damage)
 void MovingActor::recover()
 {
 	while (_actorState->getCurrentMagic() < _actorState->getMaxMagic())
-		this->scheduleOnce(schedule_selector(MagicRecover), 1.0f);	
+		this->scheduleOnce(schedule_selector(MovingActor::MagicRecover), 1.0f);
 	while (_actorState->getCurrentArmor() < _actorState->getMaxArmor())
-		this->scheduleOnce(schedule_selector(MagicRecover), 1.0f);
+		this->scheduleOnce(schedule_selector(MovingActor::MagicRecover), 1.0f);
 }
 
 void MovingActor::MagicRecover(float dt)
@@ -59,8 +65,42 @@ void MovingActor::ArmorRecover(float dt)
 	_actorState->changeCurrentArmor(_actorState->getCurrentArmor() + 1);
 }
 
+void MovingActor::updateDirection()
+{
+	if (_angle <= 2 * MINDEGREE || _angle > 14 * MINDEGREE)
+	{
+		_actordirection = Direction::RIGHT;
+	}
+	else if (_angle <= 6 * MINDEGREE)
+	{
+		_actordirection = Direction::UP;
+	}
+	else if (_angle <= 10 * MINDEGREE)
+	{
+		_actordirection = Direction::LEFT;
+	}
+	else if (_angle <= 14 * MINDEGREE)
+	{
+		_actordirection = Direction::DOWN;
+	}
+}
+
+void MovingActor::startAnimation(){}
+
+void MovingActor::moveTo(const Vec2& targetPosition)
+{
+	auto oldDirection = _actordirection;
+	_angle = ccpAngle(getPosition(), targetPosition);
+	updateDirection();
+	auto direction = ccpNormalize(ccpSub(targetPosition, getPosition()));
+	if (_actordirection != oldDirection)
+	{
+		startAnimation();
+	}
+	auto newPosition = ccpAdd(getPosition(), ccpMult(direction, _actorState->getCurrentSpeed());
+	setPosition(newPosition);
+}
+
 void MovingActor::Attack() {};
 
 void MovingActor::Buff() {};
-
-void MovingActor::GetWeapon() {};
