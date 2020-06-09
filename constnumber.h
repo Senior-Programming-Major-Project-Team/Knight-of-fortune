117250c11aa24185dee8bc0bf8adea76eeaba8e8
kiki-doyouloveme <58351085+kiki-doyouloveme@users.noreply.github.com>
2020-06-09 14:15:04 +0800

    Update constnumber.h

--- constnumber.h
+++ constnumber.h
@@ -9,11 +9,21 @@
 #define MAGIC 200				  
 #define ARMOR 5
 #define SPEED 1
+#define MINDEGREE 0.3926991
 
 enum Camp
 {
 	ME,
 	ENEMY
+};
+
+enum Direction
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	NO
 };
 
 #endif //_CONSTNUMBER_ 
