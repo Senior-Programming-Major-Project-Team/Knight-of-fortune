6933c286b9a68d23a9ca41b31245a6c78ef7205b
kiki-doyouloveme <58351085+kiki-doyouloveme@users.noreply.github.com>
2020-06-15 21:00:12 +0800

    Update HelloWorldScene.h

--- HelloWorldScene.h
+++ HelloWorldScene.h
@@ -1,27 +1,3 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #ifndef __HELLOWORLD_SCENE_H__
 #define __HELLOWORLD_SCENE_H__
 
@@ -41,11 +17,11 @@ public:
 
     virtual bool init();
 
-	void initdate();
+	void initstatedate();//初始化按键信息
 
-	void inithero();
+	void inithero();//初始化人物信息
 
-	void Heromove(Hero* hero);
+	void Heromove(Hero* hero);//人物移动
     
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
@@ -54,13 +30,13 @@ public:
 
 	virtual bool onReleaseKey(EventKeyboard::KeyCode keyCode, Event* envet, Hero*hero);
 
-	std::map<cocos2d::EventKeyboard::KeyCode, bool> keys;
+	std::map<cocos2d::EventKeyboard::KeyCode, bool> keys;//储存按键信息
 
-	bool isKeyPressed(EventKeyboard::KeyCode keyCode);
+	bool isKeyPressed(EventKeyboard::KeyCode keyCode);//判断按键是否一直被按住
 
 	void update(float delta);
 
-	bool updateState(EventKeyboard::KeyCode keyCode, int type);
+	bool updateState(EventKeyboard::KeyCode keyCode, int type);//更新按键信息
 
 	bool PressDirection();
 
