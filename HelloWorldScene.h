eca0e1f8cbf6657aee649ef77c180b13b818ede7
kiki-doyouloveme <58351085+kiki-doyouloveme@users.noreply.github.com>
2020-06-16 19:01:06 +0800

    Update HelloWorldScene.h

--- HelloWorldScene.h
+++ HelloWorldScene.h
@@ -1,18 +1,51 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
 #ifndef __HELLOWORLD_SCENE_H__
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
 #include "knight.h"
+#include "Pig.h"
+#include "LanceGoblin.h"
+#include "Flower.h"
+#include "HandGunGoblin.h"
 
 class Hero;
+class Enemy;
 
 class HelloWorld : public cocos2d::Scene
 {
 
 	CC_SYNTHESIZE(Direction, _pressdirection, PressDirection);
 	CC_SYNTHESIZE(bool, _isCanMove, IsCanMove);
+	CC_SYNTHESIZE(Hero*, _myHero, MyHero);
 
 public:
+
+	Vector<Enemy*> _enemies;
+
     static cocos2d::Scene* createScene();
 
     virtual bool init();
@@ -21,20 +54,34 @@ public:
 
 	void inithero();//初始化人物信息
 
-	void Heromove(Hero* hero);//人物移动
+	void initenemy();//初始化敌人信息
+
+	void initpig();
+
+	void initlancegoblin();
+
+	void initflower();
+
+	void inithandgungoblin();
+
+	void Heromove();//人物移动
     
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
 
-	virtual bool onPressKey(EventKeyboard::KeyCode keyCode, Event* envet, Hero*hero);
+	virtual bool onPressKey(EventKeyboard::KeyCode keyCode, Event* envet);
 
-	virtual bool onReleaseKey(EventKeyboard::KeyCode keyCode, Event* envet, Hero*hero);
+	virtual bool onReleaseKey(EventKeyboard::KeyCode keyCode, Event* envet);
 
 	std::map<cocos2d::EventKeyboard::KeyCode, bool> keys;//储存按键信息
 
 	bool isKeyPressed(EventKeyboard::KeyCode keyCode);//判断按键是否一直被按住
 
 	void update(float delta);
+
+	void updateHero(float delta);
+
+	void updateEnemy(float delta);
 
 	bool updateState(EventKeyboard::KeyCode keyCode, int type);//更新按键信息
 
@@ -46,9 +93,6 @@ public:
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
-
-protected:
-	Knight* knight = Knight::create(this, "knight", Camp::ME);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
