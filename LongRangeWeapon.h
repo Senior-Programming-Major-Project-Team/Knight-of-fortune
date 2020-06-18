3aa52c8a2e6a9af0dbf25ba777fc0d499ac6004b
kiki-doyouloveme <58351085+kiki-doyouloveme@users.noreply.github.com>
2020-06-18 15:32:03 +0800

    Create LongRangeWeapon.h

---
+++ LongRangeWeapon.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef LONG_RANGE_WEAPON
+#define LONG_RANGE_WEAPON
+#include"cocos2d.h"
+#include"Weapon.h"
+USING_NS_CC;
+
+class longRangeWeapon :public Weapon
+{
+	CC_SYNTHESIZE(INT32, _magicPoint, MagicPoint);
+	CC_SYNTHESIZE(INT32, _attackPoint, AttackPoint);
+	CC_SYNTHESIZE(float, _critRate, CritRate);
+	CC_SYNTHESIZE(String, _weaponName, WeaponName);
+public:
+	virtual bool init(HelloWorld* combatScene, std::string weaponName, Hero* hero);
+	//CREATE_FUNC(longRangeWeapon);
+	static longRangeWeapon* create(HelloWorld* combatScene, std::string weaponName, Hero* hero);
+	//static Scene* createScene();
+	
+	virtual void openFire(HelloWorld* combatScene);
+
+};
+
+
+#endif // !LONG_RANGE_WEAPON
